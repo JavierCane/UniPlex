0038b6b3cb09da3ecf50cd9bd10e22d3f5870378
Javier Ferrer <javier.mailserio@gmail.com>
2014-05-27 12:09:22 +0200

    Ejemplo asignación oferta a la vista de detalle de oferta (el título de la oferta y el nombre de la empresa los muestra dinámicamente.

--- app/controllers/students/offers/Details.java
+++ app/controllers/students/offers/Details.java
@@ -1,5 +1,7 @@
 package controllers.students.offers;
 
+import models.Oferta;
+import play.db.ebean.Model;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Students.Offers.details;
@@ -7,8 +9,12 @@ import views.html.Students.Offers.details;
 public class Details extends Controller {
 
     public static Result viewOfferDetails( String companyUrlize, String offerUrlize, long offerId ) {
+        // TODO: Comprobar urlize empresa y oferta con respecto a los del ID de la oferta, si no coinciden, 301 al correcto.
+
+        Oferta oferta = Oferta.find.byId( 1 );
+
         String headerText = "Job details for offer ID: " + offerId + ", companyUrlize: " + companyUrlize + ", offerUrlize: " + offerUrlize;
 
-        return ok( details.render( offerUrlize + " a " + companyUrlize, headerText ) );
+        return ok( details.render( offerUrlize + " a " + companyUrlize, headerText, oferta ) );
     }
 }
